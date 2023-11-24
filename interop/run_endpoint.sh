6663ac576793738f5ea1d5fb640ac5022af2d7c2
Sijie Yang <iyangsj@gmail.com>
2023-11-24 17:33:33 +0800

    Update interop testing - Add .dockerignore - Add lsquic to interop testing matrix - Fix issues found by interop testing

--- interop/run_endpoint.sh
+++ interop/run_endpoint.sh
@@ -49,6 +49,7 @@ if [ "$ROLE" == "client" ]; then
     REQS=($REQUESTS)
 
     CLIENT_ARGS="--dump-path ${DOWNLOAD_DIR} --keylog-file $SSLKEYLOGFILE --log-level TRACE --max-concurrent-requests ${#REQS[@]}"
+    CLIENT_ALPN="--alpn hq-interop"
     case $TESTCASE in
     resumption)
         CLIENT_ARGS="$CLIENT_ARGS --session-file=session.bin"
@@ -56,9 +57,13 @@ if [ "$ROLE" == "client" ]; then
     zerortt)
         CLIENT_ARGS="$CLIENT_ARGS --session-file=session.bin --enable-early-data"
         ;;
+    http3)
+        CLIENT_ALPN="--alpn h3"
+        ;;
     *)
         ;;
     esac
+    CLIENT_ARGS="$CLIENT_ARGS $CLIENT_ALPN"
 
     case $TESTCASE in
     multiconnect|resumption)
@@ -76,7 +81,7 @@ if [ "$ROLE" == "client" ]; then
         ;;
     esac
 elif [ "$ROLE" == "server" ]; then
-    SERVER_ARGS="-c /certs/cert.pem -k /certs/priv.key --listen [::]:443 --root $ROOT_DIR --keylog-file $SSLKEYLOGFILE"
+    SERVER_ARGS="-c /certs/cert.pem -k /certs/priv.key --listen [::]:443 --root $ROOT_DIR --log-level TRACE --keylog-file $SSLKEYLOGFILE"
     case $TESTCASE in
     retry)
         SERVER_ARGS="$SERVER_ARGS --enable-retry"
