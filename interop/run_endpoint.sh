5056b14389503b4af41270808e5eb94288c87488
xiaofei0800 <54530729+xiaofei0800@users.noreply.github.com>
2024-01-24 15:11:02 +0800

    tquic_client supports early data. (#155)

--- interop/run_endpoint.sh
+++ interop/run_endpoint.sh
@@ -21,9 +21,9 @@ set -x
 /setup.sh
 
 case "$TESTCASE" in
-handshake|http3|resumption|ipv6|goodput|crosstraffic|transfer|transferloss|transfercorruption|multiplexing|longrtt|chacha20|blackhole|retry|handshakeloss|handshakecorruption|multiconnect)
+handshake|http3|resumption|zerortt|ipv6|goodput|crosstraffic|transfer|transferloss|transfercorruption|multiplexing|longrtt|chacha20|blackhole|retry|handshakeloss|handshakecorruption|multiconnect)
     ;;
-zerortt|chacha20)
+chacha20)
     if [ "$ROLE" == "client" ]; then
         exit 127
     fi
