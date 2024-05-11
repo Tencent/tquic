a41916c79f61279bfc4a61fe33181da0293dbb51
xiaofei0800 <nemo_00o@hotmail.com>
2024-05-11 15:30:08 +0800

    Optimize benchmark workflow.

--- .github/workflows/plot-goodput.py
+++ .github/workflows/plot-goodput.py
@@ -7,7 +7,7 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 
-# QUIC implements
+# QUIC implementations
 IMPLS = ["tquic", "gquiche", "lsquic", "picoquic", "quiche"]
 
 # Different file sizes
